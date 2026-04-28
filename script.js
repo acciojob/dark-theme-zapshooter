08a0faadfc17d9cff5ee269f51ae0d66e32dce45
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-04-28 23:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+function swapTheme() {
+  const app = document.getElementById("app");
+  const swap = document.getElementById("swap");
+
+  if (app.classList.contains("day")) {
+    app.classList.replace("day", "night");
+    swap.classList.replace("button_day", "button_night");
+  } else {
+    app.classList.replace("night", "day");
+    swap.classList.replace("button_night", "button_day");
+  }
+}
